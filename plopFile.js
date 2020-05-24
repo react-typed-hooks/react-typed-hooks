57189a707ce32486c0f3a2ddac7be5ce2a3af3e6
Christopher Dierkens <cjdierkens@gmail.com>
2020-05-23 18:54:01 -0600

    Initial tooling commit with lerna, plop, jest, and testing-library

---
+++ plopFile.js
@@ -0,0 +1,24 @@
+const path = require("path");
+
+module.exports = function (plop) {
+  plop.setGenerator("hook", {
+    description: "Add a Hook Package",
+    prompts: [
+      {
+        type: "input",
+        name: "name",
+        message: "Input a name for the hook:",
+        default: "useHook",
+      },
+    ],
+    actions: [
+      {
+        type: "addMany",
+        destination: path.join(__dirname, "packages", "/{{ camelCase name }}"),
+        templateFiles: path.join(__dirname, ".plop", "hook", "**/*"),
+        base: path.join(__dirname, ".plop", "hook"),
+        skipIfExists: true,
+      },
+    ],
+  });
+};
