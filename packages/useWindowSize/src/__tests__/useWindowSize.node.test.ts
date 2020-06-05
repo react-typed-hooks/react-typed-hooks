bbbbd25b6bf909867ec363471fff3a7cf5ef2051
Christopher Dierkens <christopher.dierkens@formidable.com>
2020-06-05 16:47:31 -0600

    Add alpha useWindowSize

---
+++ packages/useWindowSize/src/__tests__/useWindowSize.node.test.ts
@@ -0,0 +1,19 @@
+/**
+ * @jest-environment node
+ */
+
+import { renderHook } from "@testing-library/react-hooks";
+import useWindowSize from "../useWindowSize";
+
+describe("node", () => {
+  describe("useWindowSize", () => {
+    it("should return undefined height and width", () => {
+      const { result } = renderHook(() => useWindowSize());
+
+      expect(result.current).toEqual({
+        height: undefined,
+        width: undefined,
+      });
+    });
+  });
+});
---
+++ packages/useWindowSize/src/__tests__/useWindowSize.test.ts
@@ -0,0 +1,53 @@
+import { act, renderHook } from "@testing-library/react-hooks";
+import useWindowSize from "../useWindowSize";
+
+const defaultHeight = window.innerHeight;
+const defaultWidth = window.innerHeight;
+
+describe("useWindowSize", () => {
+  afterEach(() => {
+    Object.defineProperties(window, {
+      innerWidth: { value: defaultWidth },
+      innerHeight: { value: defaultHeight },
+    });
+  });
+
+  beforeEach(() => {
+    Object.defineProperties(window, {
+      innerWidth: { value: 700 },
+      innerHeight: { value: 1000 },
+    });
+  });
+
+  it("should return height and width", () => {
+    const { result } = renderHook(() => useWindowSize());
+
+    expect(result.current).toEqual({
+      width: 700,
+      height: 1000,
+    });
+  });
+
+  it("should update height and width on resize", () => {
+    const { result } = renderHook(() => useWindowSize());
+
+    expect(result.current).toEqual({
+      width: 700,
+      height: 1000,
+    });
+
+    Object.defineProperties(window, {
+      innerWidth: { value: 768 },
+      innerHeight: { value: 1024 },
+    });
+
+    act(() => {
+      window.dispatchEvent(new Event("resize"));
+    });
+
+    expect(result.current).toEqual({
+      width: 768,
+      height: 1024,
+    });
+  });
+});
